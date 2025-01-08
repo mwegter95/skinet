d294e88cad50e3ecf516e075fff69bda112f5a38
mwegter95 <mwegter95@gmail.com>
2025-01-08 13:59:09 -0600

    initial commit

---
+++ infrastructure/config/ProductConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using core.entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.config;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+    }
+}
---
+++ infrastructure/data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using core.entities;
+using infrastructure.config;
+using Microsoft.EntityFrameworkCore;
+
+namespace infrastructure.data;
+
+public class StoreContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
